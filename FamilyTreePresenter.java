0154403a1a9ac6c409a1853fd266a2e10ac705b6
gatlinglolx <shahov199@gmail.com>
2023-12-12 21:11:30 +0300

    FamilyTreePresenter_1

---
+++ FamilyTreePresenter.java
@@ -0,0 +1,6 @@
+public interface FamilyTreePresenter {
+    void addPerson(Person person);
+    void removePerson(Person person);
+    void updatePerson(Person person);
+    void getAllPersons();
+}
